b739880fb68939c4e9b9875b6c94eaef56999ab2
Dylan Martin <dylanmartin2523@gmail.com>
2020-11-09 16:53:32 -0500

    M2

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.listen(port, () =>
   console.log('Example app listening on port ' + port)
 );
 
+
+this.server.use('/', express.static('client'));
+
 app.get('/', (req, res) => {
     res.send("OOF");
 });
